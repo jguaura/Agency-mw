b916da148da954b0f4eaaa8d8944da95daef7fa3
Jose Guaura <jguaura@8amarketing.com>
2020-11-10 20:09:25 -0300

    added imgs from cdn

--- src/components/Case/Case.jsx
+++ src/components/Case/Case.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import curology from '../../assets/curology-min.png'
-import lumin from '../../assets/lumin-min.png'
-import workspace from '../../assets/yourspace-min.png'
+const curology = "https://res.cloudinary.com/jguaura/image/upload/c_scale,w_600/v1605049210/Agency/curology-min_pbwokv.webp"
+const lumin = "https://res.cloudinary.com/jguaura/image/upload/c_scale,w_600/v1605049220/Agency/lumin-min_nxa4ub.webp"
+const workspace = "https://res.cloudinary.com/jguaura/image/upload/c_scale,w_600/v1605049225/Agency/yourspace-min_ifzkuq.webp"
 
 const Case = ({ item }) => {
     const { id, subtitle, title, img } = item
